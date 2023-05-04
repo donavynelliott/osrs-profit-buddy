a48853e26a4f7d8d05550945e503cb36f4a2cf82
Don Elliott <donavyn.elliott@gmail.com>
2023-05-04 14:59:03 -0700

    Added badges to search results to display quick profits

--- app/Http/Controllers/ItemQueryController.php
+++ app/Http/Controllers/ItemQueryController.php
@@ -14,12 +14,14 @@ class ItemQueryController extends Controller
      * @param  int  $minHourlyVolume
      * @return \Illuminate\Support\Collection
      */
-    public static function getTopItemsWithHighestProfitMargin($limit = 100, $minHourlyVolume = 10)
+    public static function getTopItemsWithHighestProfitMargin($limit = 100, $minHourlyVolume = 10, $maxItemPrice = 100000000)
     {
         $minHourlyVolume = request()->input('min-hourly-volume', $minHourlyVolume);
+        $maxItemPrice = request()->input('max-item-price', $maxItemPrice);
 
         $items = Item::selectRaw('*, (high - low - LEAST(FLOOR(high*0.01), 5000000)) AS profit_margin')
             ->havingRaw('highPriceVolume + lowPriceVolume >= ?', [$minHourlyVolume])
+            ->havingRaw('avgLowPrice <= ?', [$maxItemPrice])
             ->orderByDesc('profit_margin')
             ->limit($limit)
             ->get();
--- resources/views/items/search.blade.php
+++ resources/views/items/search.blade.php
@@ -7,9 +7,13 @@
 
 <div class="list-group list-group-flush">
     @foreach ($items as $item)
+    <?php $marginData = $item->getProfitMarginWithTax(); ?>
     <div class="list-group-item list-group-item-action">
         <img src="{{ $item->getLocalItemImage() }}" style="max-width: 3rem;" alt="{{ $item->name }}">
         <a href=" {{ route('items.show', $item->item_id) }}">{{ $item->name }}</a>
+        <span class="badge bg-primary rounded-pill">Margin: {{ number_format($marginData['margin']) }}</span>
+        <span class="badge bg-success rounded-pill">Profit: {{ number_format($marginData['profit']) }}</span>
+
     </div>
     @endforeach
 </div>
