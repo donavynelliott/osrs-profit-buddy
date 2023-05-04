529e8263899d892ea10929663f7a897db90bcacc
Don Elliott <donavyn.elliott@gmail.com>
2023-05-04 06:42:37 -0700

    Create blade templates and compiled resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+
+// resources/js/app.js
+import * as bootstrap from 'bootstrap';
+import * as bootstrapIcons from 'bootstrap-icons';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @import "~bootstrap/scss/bootstrap";
+                    @import "~bootstrap-icons/font/bootstrap-icons.scss";
+                `
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(new URL('./node_modules/bootstrap', import.meta.url).pathname),
+            '~bootstrap-icons': path.resolve(new URL('./node_modules/bootstrap-icons', import.meta.url).pathname),
+            'bootstrap-icons': path.resolve(new URL('./node_modules/bootstrap-icons', import.meta.url).pathname, 'font/fonts/bootstrap-icons.woff2?v=1.5.0'),
+        }
+    }
 });
