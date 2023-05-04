dbdf8e88201fd7adfd9cbd43b22adeffa5e35811
Don Elliott <donavyn.elliott@gmail.com>
2023-05-04 11:28:05 -0700

    Modified Item class and templates to now use local images

--- resources/views/items/search.blade.php
+++ resources/views/items/search.blade.php
@@ -8,7 +8,7 @@
 <div class="list-group list-group-flush">
     @foreach ($items as $item)
     <div class="list-group-item list-group-item-action">
-        <img src="{{ $item->getOSRSItemImage() }}" style="max-width: 3rem;" alt="{{ $item->name }}">
+        <img src="{{ $item->getLocalItemImage() }}" style="max-width: 3rem;" alt="{{ $item->name }}">
         <a href=" {{ route('items.show', $item->item_id) }}">{{ $item->name }}</a>
     </div>
     @endforeach
