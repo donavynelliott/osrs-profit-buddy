81c5c315365773df6f0930037322a708d8a3fc42
Don Elliott <donavyn.elliott@gmail.com>
2023-05-04 07:35:16 -0700

    Create basic item display and show information about that item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -27,4 +27,19 @@ class Item extends Model
         'avgLowPrice',
         'lowPriceVolume'
     ];
+
+    function getOSRSItemImage()
+    {
+        $item_url = "https://secure.runescape.com/m=itemdb_oldschool/1683109788382_obj_big.gif?id=" . $this->item_id;
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', $item_url);
+
+        if ($response->getStatusCode() == 200) {
+            return $item_url;
+        } else {
+            return "https://via.placeholder.com/36";
+        }
+    }
+
 }
