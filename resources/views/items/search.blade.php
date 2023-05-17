ead2b286662465f48e0979d9a39d2383d95c8677
Don Elliott <donavyn.elliott@gmail.com>
2023-05-17 05:49:27 -0700

    Made all tables use datatables

--- resources/views/items/search.blade.php
+++ resources/views/items/search.blade.php
@@ -4,33 +4,52 @@
 
 @section('content')
 @if ($items->count())
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Item Name</th>
-            <th scope="col">High</th>
-            <th scope="col">Low</th>
-            <th scope="col">Margin</th>
-            <th scope="col">Profit</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($items as $item)
-        <?php $marginData = $item->getProfitMarginWithTax(); ?>
-        <tr>
-            <td>
-                <div class="d-flex align-items-center">
-                    <img src="{{ $item->getLocalItemImage() }}" class="img-fluid rounded me-2" alt="{{ $item->name }}" style="width: 24px; height: 24px;">
-                    <a href="{{ route('items.show', $item->item_id) }}">{{ $item->name }}</a>
-                </div>
-            </td>
-            <td>{{ number_format($item->high) }}</td>
-            <td>{{ number_format($item->low) }}</td>
-            <td>{{ number_format($marginData['margin']) }}</td>
-            <td>{{ number_format($marginData['profit']) }}</td>
-        </tr>
-        @endforeach
-</table>
+<div class="card mb-4">
+    <div class="card-header">
+        <i class="fas fa-table me-1"></i>
+    </div>
+    <div class="card-body">
+        <table id="items-table" class="datatable">
+            <thead>
+                <tr>
+                    <th scope="col">Item Name</th>
+                    <th scope="col">High</th>
+                    <th scope="col">Low</th>
+                    <th scope="col">Margin</th>
+                    <th scope="col">Profit</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                <?php $marginData = $item->getProfitMarginWithTax(); ?>
+                <tr>
+                    <td>
+                        <div class="d-flex align-items-center">
+                            <img src="{{ $item->getLocalItemImage() }}" class="img-fluid rounded me-2" alt="{{ $item->name }}" style="width: 24px; height: 24px;">
+                            <a href="{{ route('items.show', $item->item_id) }}">{{ $item->name }}</a>
+                        </div>
+                    </td>
+                    <td>{{ number_format($item->high) }}</td>
+                    <td>{{ number_format($item->low) }}</td>
+                    <td>{{ number_format($marginData['margin']) }}</td>
+                    <td>{{ number_format($marginData['profit']) }}</td>
+                </tr>
+                @endforeach
+        </table>
+    </div>
+</div>
+
+<script>
+    //Enable datatables
+    $(document).ready(function() {
+        $('#items-table').DataTable({
+            "order": [
+                [4, "desc"]
+            ] // 4 is the index of the Profit column
+        });
+    });
+</script>
+
 @else
 <p>No items found.</p>
 @endif
--- resources/views/profit-calcs/item-sets.blade.php
+++ resources/views/profit-calcs/item-sets.blade.php
@@ -3,38 +3,46 @@
 @section('title', 'Potential Item Set Profits')
 
 @section('content')
-<p>
+<p class="lead">
     This page is for finding sets that can be profited from packing/unpacking and reselling.
 </p>
-<table id="item-set-table">
-    <thead>
-        <tr>
-            <th>Item Set</th>
-            <th>Items</th>
-            <th>Pack Margin</th>
-            <th>Profit</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($itemSets as $set_item_id => $itemSet)
-        <tr>
-            <td>
-                <div class="d-flex align-items-center">
-                    <a href="{{ route('items.show', $set_item_id) }}">{{ $itemSet['set_item']->name }}</a>
-                </div>
-            </td>
-            <td>
-                @foreach ($itemSet['items'] as $item)
-                <a style="text-decoration: none;" href="{{ route('items.show', $item->item_id) }}">
-                    <img src="{{ $item->getLocalItemImage() }}" class="img-fluid rounded me-2" alt="{{ $item->name }}" style="width: 48px; height: 48px;">
-                </a>
+<div class="card mb-4">
+    <div class="card-header">
+        <i class="fas fa-table me-1"></i>
+    </div>
+    <div class="card-body">
+        <table id="item-set-table" class="datatable">
+            <thead>
+                <tr>
+                    <th scope="col">Item Set</th>
+                    <th scope="col">Items</th>
+                    <th scope="col">Pack Margin</th>
+                    <th scope="col">Profit</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($itemSets as $set_item_id => $itemSet)
+                <tr>
+                    <td scope="row">
+                        <div class="d-flex align-items-center">
+                            <a href="{{ route('items.show', $set_item_id) }}">{{ $itemSet['set_item']->name }}</a>
+                        </div>
+                    </td>
+                    <td>
+                        @foreach ($itemSet['items'] as $item)
+                        <a style="text-decoration: none;" href="{{ route('items.show', $item->item_id) }}">
+                            <img src="{{ $item->getLocalItemImage() }}" class="img-fluid rounded me-2" alt="{{ $item->name }}" style="width: 48px; height: 48px;">
+                        </a>
+                        @endforeach
+                    </td>
+                    <td>{{ number_format($itemSet['profit_margin']['margin']) }}</td>
+                    <td>{{ number_format($itemSet['profit_margin']['profit']) }}</td>
+                </tr>
                 @endforeach
-            </td>
-            <td>{{ number_format($itemSet['profit_margin']['margin']) }}</td>
-            <td>{{ number_format($itemSet['profit_margin']['profit']) }}</td>
-        </tr>
-        @endforeach
-</table>
+        </table>
+    </div>
+</div>
+
 <script>
     $(document).ready(function() {
         $('#item-set-table').DataTable();
